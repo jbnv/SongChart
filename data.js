ea59a0a268607f889e3a896439112ead9d14b833
Jay Bienvenu <github@bienv.com>
2014-09-07 19:29:23 -0500

    First version of application.
    Show some hard-coded data.

---
+++ data.js
@@ -0,0 +1,22 @@
+//<script type="text/javascript" src="https://jsapi.wikidot.com/local--code/js/4"></script>
+
+//TODO Get song data from playlists site.
+var songData = [
+	{
+		artist: 'Extreme',
+		title: 'Hole Hearted'
+	},{
+		artist: 'Karyn White',
+		title: 'Romantic'
+	},{
+		artist: 'Tears for Fears',
+		title: "Sowing the Seeds of Love"
+	}
+];
+
+//TODO Get score data from playlists site.
+var scoreData = [
+	{ songId: 0, date: '1991-08', score: 90 },
+	{ songId: 0, date: '1991-09', score: 100 },
+	{ songId: 1, date: '1991-09', score: 99 }
+];
---
+++ songChartApp.js
@@ -0,0 +1,1 @@
+var songChartApp = angular.module('songChartApp', ['ui.bootstrap']);
