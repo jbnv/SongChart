a8bd7a8a1a4521cc8e6bcb7182acb74bd64eb000
Jay Bienvenu <github@bienv.com>
2014-09-09 16:20:02 -0500

    Take WD API code out of the controller.

--- songChartController.js
+++ songChartController.js
@@ -1,32 +1,7 @@
 songChartApp.controller('songChartController', ['$scope', '$filter', function($scope, $filter) {
 
-	var apikey = "r0JF5IqvvWXmr3JRulYGNE0qd4BKeiMN";
-	
 	var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-
-/*	// Get all of the pages from playlists in the 'score' category.
-	scorePageList = jsAPI(
-		"pages.select",
-		{"site": "playlists", "categories": ["score"]},
-		apikey
-	); // returns array of page names
-	$scope.scoreObjectArray = [];
-	for (var page in scorePageList) {
-		// Get the particular page.
-		wdPageObject = jsAPI(
-			"pages.get_one",
-			{"site": "playlists", "page": page},
-			apikey
-		);
-		// Extract the pertinent form data.
-		// parent = song
-		scoreObject = {};
-		scoreObject.song = wdPageObject.parent_fullname; //TODO Get song information and artist name.
-		scoreObject.date = wdPageObject.date;
-		scoreObject.score = wdPageObject.score;
-		$scope.scoreObjectArray.push(scoreObject);
-	}
-*/	
+	
 	$scope.init = function() {
 		$scope.songObjectArray = songData;
 		$scope.scoreObjectArray = scoreData;
