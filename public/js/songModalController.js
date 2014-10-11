5b74285c2b0281a2b77489006fbc61d8293b7302
Jay Bienvenu <github@bienv.com>
2014-10-11 18:26:14 -0500

    Pivot ranks in detail popup.

--- public/js/songModalController.js
+++ public/js/songModalController.js
@@ -10,5 +10,17 @@ function SongModalController(
 	$scope.ok = function () {
 		$modalInstance.close();
 	};
+	
+	$scope.pivot = function(pointRanks) {
+		returnValue = [];
+		for (index in pointRanks) {
+			if (index % 4 == 0) {
+				month = [];
+				returnValue.push(month);
+			}
+			month.push(pointRanks[index].value);
+		}
+		return returnValue;
+	}
 
 }
